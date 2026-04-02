987ee852dab2836dfbdee31f415f9877e634cc20
ankitmavani <mavaniankit67@gmail.com>
2026-04-02 10:51:54 +0000

    while program working

--- Basic_program/while_loop/02_print_numbers_from_10_down_to_1_in_reverse_order.js
+++ Basic_program/while_loop/02_print_numbers_from_10_down_to_1_in_reverse_order.js
@@ -2,11 +2,19 @@
 // Problem: Print numbers from 10 down to 1 in reverse order
 // Category: while_loop
 // Difficulty: Easy
-// Status: ❌ Not Solved
+// Status: ✅ Solved
 // ==========================================
 
 function solve() {
   // TODO: implement
+  let i=10
+  let output=''
+  while(i>0){
+    output+=i
+    if(i>1) output+=' '
+    i-=1
+  }
+  console.log(output)
 }
 
 solve();
