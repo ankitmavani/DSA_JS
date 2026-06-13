23cda1951fa37bbd16c994f59d317b47f241cbda
ankitmavani <mavaniankit67@gmail.com>
2026-06-13 13:08:26 +0000

    complete phase_2_q1

--- Basic_program/phase_02_do_while_loop/01_print_numbers_from_1_to_10.js
+++ Basic_program/phase_02_do_while_loop/01_print_numbers_from_1_to_10.js
@@ -2,11 +2,16 @@
 // Problem: Print numbers from 1 to 10
 // Category: do_while_loop
 // Difficulty: Easy
-// Status: ❌ Not Solved
+// Status: ✅ Solved
 // ==========================================
 
-function solve() {
+function solve(n) {
   // TODO: implement
+  let index = 1;
+  do {
+    console.log(index);
+    index++;
+  } while (index <= 10);
 }
 
-solve();
+solve(10);
